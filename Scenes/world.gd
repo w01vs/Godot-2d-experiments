c4d525802c911d00c8a3fab9f537b0ee5305a4fb
zWolv <samoht684@gmail.com>
2024-02-11 22:25:12 +0100

    camera follows player horizontally

---
+++ Scenes/world.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var camera: Camera2D = $Cam
+@onready var player: Player = $Player
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	camera.set_position(Vector2(player.get_position().x, 0))

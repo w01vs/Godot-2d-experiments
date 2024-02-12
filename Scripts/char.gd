a30d242f2d9a0e7c93533398df43aa4288e82fa3
zWolv <samoht684@gmail.com>
2024-02-12 10:25:37 +0100

    dash

--- Scripts/char.gd
+++ Scripts/char.gd
@@ -1,8 +1,9 @@
 class_name Player extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED: float = 300.0
+const DASH_SPEED: float = 1000.0
+const JUMP_VELOCITY: float = -400.0
 
 @export var jumps: int
 var jumps_left: int
@@ -22,13 +23,18 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and jumps_left > 0:
 		velocity.y += JUMP_VELOCITY
 		jumps_left -= 1
-
+	
+	# Dash
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = lerp(velocity.x, direction * SPEED, 0.25)
+		# dash needs to lerp x position.
+		if Input.is_action_just_pressed("dash") and is_on_floor():
+			velocity.x += 1000 * direction
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = lerp(velocity.x, 0.0, 0.05)
+	
 	move_and_slide()

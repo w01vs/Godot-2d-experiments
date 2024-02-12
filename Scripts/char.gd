207a3f5e807d88d7a33e6235e65f90cb802b8bdf
zWolv <samoht684@gmail.com>
2024-02-12 12:35:21 +0100

    fast dash when landing

--- Scripts/char.gd
+++ Scripts/char.gd
@@ -6,7 +6,10 @@ const DASH_SPEED: float = 1000.0
 const JUMP_VELOCITY: float = -400.0
 
 @export var jumps: int
+@onready var timer = $Timer
+
 var jumps_left: int
+var was_on_floor: bool = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -16,6 +19,12 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	
+	# start timer for faster dash when landing again.
+	if is_on_floor() and not was_on_floor:
+		print_debug("speedy dash timer")
+		timer.start(0.4)
+		was_on_floor = true
 
 	# Handle jump.
 	if is_on_floor():
@@ -33,8 +42,14 @@ func _physics_process(delta):
 		velocity.x = lerp(velocity.x, direction * SPEED, 0.25)
 		# dash needs to lerp x position.
 		if Input.is_action_just_pressed("dash") and is_on_floor():
-			velocity.x += 1000 * direction
+			if timer.time_left > 0:
+				velocity.x += 2500 * direction
+				print_debug("fast dash")
+			else:
+				velocity.x += 1000 * direction
+				print_debug("normal dash")
 	else:
 		velocity.x = lerp(velocity.x, 0.0, 0.05)
 	
+	was_on_floor = is_on_floor()
 	move_and_slide()
